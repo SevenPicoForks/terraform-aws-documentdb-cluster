1aaafd2e212acd4a286479ceada806ec7268df93
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2024-01-02 23:42:29 +0530

    fixed output

--- examples/complete/_outputs.tf
+++ examples/complete/_outputs.tf
@@ -1,10 +1,10 @@
 output "public_subnet_cidrs" {
-  value       = module.subnets.public_subnet_cidrs
+  value       = module.vpc_subnets.public_subnet_cidrs
   description = "Public subnet CIDRs"
 }
 
 output "private_subnet_cidrs" {
-  value       = module.subnets.private_subnet_cidrs
+  value       = module.vpc_subnets.private_subnet_cidrs
   description = "Private subnet CIDRs"
 }
 
