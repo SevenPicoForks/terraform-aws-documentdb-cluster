c0bf7ef8ae42ccf728dd1dac41b8618e7987fe63
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2024-01-08 19:44:06 +0530

    fixed outputs

--- modules/events/_outputs.tf
+++ modules/events/_outputs.tf
@@ -1,10 +1,10 @@
 output "id" {
-  value       = try(aws_docdb_event_subscription.ddb_event_subscription[0].id, "")
+  value       = try(aws_docdb_event_subscription.ddb_events_subscription[0].id, "")
   description = "Name of the DocumentDB event notification subscription."
 }
 
 output "arn" {
-  value       = try(aws_docdb_event_subscription.ddb_event_subscription[0].arn, "")
+  value       = try(aws_docdb_event_subscription.ddb_events_subscription[0].arn, "")
   description = "Amazon Resource Name (ARN) of the DocumentDB event notification subscription."
 }
 
